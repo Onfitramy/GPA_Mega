e25a9ae624bc68880e8c165a70793368346a0c93
Marcus <marwie04@gmx.de>
2025-03-31 16:09:50 +0200

    Basics for LSM6DSR IMU

---
+++ Primary/Drivers/Components/IMU1/LSM6DSR.c
@@ -0,0 +1,6 @@
+#include "LSM6DSR.h"
+#include "stdbool.h"
+
+bool LSM6DSR_SelfTest(void){
+
+}
---
+++ Primary/Drivers/Components/IMU1/LSM6DSR.h
@@ -0,0 +1,8 @@
+#ifndef LSM6DSR_H_
+#define LSM6DSR_H_
+
+#include "stm32h7xx_hal.h"
+
+#define WHO_AM_I_REG
+
+#endif /* LSM6DSR_H_ */
